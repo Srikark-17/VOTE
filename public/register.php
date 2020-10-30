79cc55e26de6034118102cf61b834daad6b66609
Om Joshi <joshi.om89@bloomfield.org>
2020-10-30 16:38:04 -0400

    Adding in firebase project hosting folder

---
+++ public/register.php
@@ -0,0 +1,22 @@
+<?php
+$base_url = 'https://api.typingdna.com/save/:id';
+$apiKey= '{079ecadfe08241731f9777dfc3033267}';
+$apiSecret ='{0f251ae38b0d26e133757fd82b80e27e}';
+$id = $_COOKIE["Username"];
+$tp = $_COOKIE["TypingPattern"];
+echo 'console.log($id)';
+echo 'console.log($tp)';
+$ch = curl_init(sprintf($base_url, 'save', $id));
+$data = array('tp' => $tp);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_USERPWD, $apiKey . ":" . $apiSecret);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data) . "\n");
+
+$response = curl_exec($ch);
+curl_close($ch);
+var_dump($response);
+?>
